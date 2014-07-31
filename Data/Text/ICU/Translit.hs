91e932d63f4e9d7e1bc6ecd9d78d0fe674e4bd8b
Antonio Nikishaev <a@lelf.me>
2014-07-31 18:50:48 +0000

    pure trans & transliterate funs

--- Data/Text/ICU/Translit.hs
+++ Data/Text/ICU/Translit.hs
@@ -1,9 +1,18 @@
 
-module Data.Text.ICU.Translit
-    (module Data.Text.ICU.Translit.Internal) where
+module Data.Text.ICU.Translit where
 
 
-import Data.Text.ICU.Translit.Internal
+import Data.Text.ICU.Translit.Internal as IO
+import System.IO.Unsafe
+import Data.Text
 
+
+
+trans :: Text -> Transliterator
+trans t = unsafePerformIO $ IO.transliterator t
+
+
+transliterate :: Transliterator -> Text -> Text
+transliterate tr txt = unsafePerformIO $ IO.transliterate tr txt
 
 
