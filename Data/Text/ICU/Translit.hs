3079dbda58b70f8fb46896387f2369f8e708b884
Antonio Nikishaev <a@lelf.me>
2014-08-02 22:58:03 +0000

    Translit: +docs

--- Data/Text/ICU/Translit.hs
+++ Data/Text/ICU/Translit.hs
@@ -1,3 +1,24 @@
+-- |
+--  Module:     Data.Text.ICU.Translit
+--  License:    BSD-style
+--  Maintainer: me@lelf.lu
+-- 
+-- This module provides the bindings to the transliteration features
+-- by the ICU (International Components for Unicode) library.
+-- 
+-- >>> IO.putStrLn $ transliterate (trans "name-any; ru") "\\N{RABBIT FACE} Nu pogodi!"
+-- 🐰 Ну погоди!
+-- 
+-- >>> IO.putStrLn $ transliterate (trans "nl-title") "gelderse ijssel"
+-- Gelderse IJssel
+-- 
+-- >>> IO.putStrLn $ transliterate (trans "ja") "Amsterdam"
+-- アムステルダム
+-- 
+-- More information about the rules is
+-- <http://userguide.icu-project.org/transforms/general here>.
+
+
 
 module Data.Text.ICU.Translit
     (IO.Transliterator, trans, transliterate) where
@@ -7,11 +28,10 @@ import System.IO.Unsafe
 import Data.Text
 
 
--- | Construct new transliterator by name. Will throw error if there's
--- no such transliterator
+-- | Construct new transliterator by name. Will throw an error if
+-- there is no such transliterator
 trans :: Text -> IO.Transliterator
 trans t = unsafePerformIO $ IO.transliterator t
-
 
 
 -- | Transliterate the text using the transliterator
