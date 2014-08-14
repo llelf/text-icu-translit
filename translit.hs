85ccea077bc4c9cd58b04f8619d2761f8b0fdf82
Antonio Nikishaev <a@lelf.me>
2014-08-14 18:26:31 +0000

    translit on pipes

--- translit.hs
+++ translit.hs
@@ -6,19 +6,31 @@ import qualified Data.Text as T
 import System.IO
 import Control.Monad
 
+import Pipes
+import Pipes.Group as PG
+import Pipes.Prelude as Pipes
+import Pipes.Text.IO as PT
+import Pipes.Text as PT
+import Lens.Family2
+
 main = do args <- getArgs
           case args of
             [rule] ->
-                go $ fun (T.pack rule)
+                go' $ fun (T.pack rule)
             _ ->
                 error "Usage"
     where
       fun :: T.Text -> T.Text -> T.Text
       fun rule = transliterate (trans rule)
 
+      go' f = runEffect $
+              over PT.lines (PG.maps (>-> Pipes.map f)) PT.stdin >-> PT.stdout
+
       go :: (T.Text -> T.Text) -> IO ()
       go f = do e <- isEOF
                 unless e $ do s <- T.getLine
                               T.putStrLn (f s)
                               go f
+
+
 
